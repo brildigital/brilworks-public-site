995101a3f0891f98e0bc01c6a72e3b5906e41896
Manish5900 <manish.s@brilworks.com>
2024-02-13 18:01:54 +0530

    Changes for user content

---
+++ src/app/dashboard/[slug]/page.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { getStoryblokApi } from "@storyblok/react/rsc";
+import ResourceData from "@/app/components/Dashboard/ResourceData";
+
+export default async function Page(props) {
+  const { params } = props || {};
+  const { props: data } = await fetchData(params);
+
+  if (!data) {
+    return null;
+  }
+
+  return (
+    <div className="bg-white">
+      <ResourceData data={data.story} />
+    </div>
+  );
+}
+
+
+export const fetchData = async (params) => {
+  const storyblokApi = getStoryblokApi();
+
+  let sbParams = {
+    version: process.env.NEXT_PUBLIC_STORYBLOK_VERSION,
+    resolve_links: "url",
+  };
+
+  const { data } = await storyblokApi.get(
+    `cdn/stories/dashboard/${params?.slug}`,
+    sbParams
+  );
+
+  return {
+    props: {
+      story: data ? data.story : false,
+    },
+    revalidate: 3600,
+  };
+};
