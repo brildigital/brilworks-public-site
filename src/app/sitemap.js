1b0fbf517fb011c7d6dc1990136c8a1c3cd4a206
Manish5900 <manish.s@brilworks.com>
2024-06-18 16:17:37 +0530

    Sitemap changes

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -4,11 +4,11 @@ import { sitemapData } from "./components/lib/constants";
 
 export async function generateSitemaps() {
   // Fetch the total number of products and calculate the number of sitemaps needed
-  return [{ id: 0 }, { id: 1 }, { id: 2 }, { id: 3 }];
+  return [{ id: 1 }, { id: 2 }, { id: 3 }];
 }
 
 export default async function sitemap({ id }) {
-  if (id === 0) {
+  if (id === 1) {
     const staticPagesData = sitemapData.map((data) => {
       return {
         url: `${data?.loc}`,
@@ -28,15 +28,9 @@ export default async function sitemap({ id }) {
   const blogListData = await getblog();
 
   const blog = blogListData.map((story) => {
-    const publishedDateStr = story?.content?.Published;
-    const dateObj = new Date(publishedDateStr) || new Date();
-    if (isNaN(dateObj.getTime())) {
-      throw new Error(`Invalid date: ${publishedDateStr}`);
-    }
-    const adjustedDate = addMinutes(dateObj, 330);
     return {
       url: `${process.env.NEXT_PUBLIC_BASE_URL}${story?.full_slug}/`,
-      lastModified: adjustedDate,
+      lastModified: story?.published_at || new Date(),
     };
   });
 
