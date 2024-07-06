37a8ef30c54f783a9525dac15e448865d561e5e2
shivsagar <shiv.s@brilworks.com>
2024-07-06 18:51:28 +0530

    updatd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 
-const withMT = require("@material-tailwind/react/utils/withMT");
 /** @type {import('tailwindcss').Config} */ 
+const withMT = require("@material-tailwind/react/utils/withMT");
+
 module.exports = withMT({
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
