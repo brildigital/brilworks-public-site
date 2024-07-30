7ed22e2780394512866c2e6651956a9250b39f56
shivsagar <shiv.s@brilworks.com>
2024-07-30 14:59:53 +0530

    updated code

--- src/app/components/RapidDigitization/index.js
+++ src/app/components/RapidDigitization/index.js
@@ -2,11 +2,15 @@
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import dynamic from "next/dynamic";
-import { formattedDate, scrollEffect } from "../lib/commonFunction";
+import { scrollEffect } from "../lib/commonFunction";
 import RapidDigitizationBanner from "./RapidDigitizationBanner";
 import { useMediaQuery } from "react-responsive";
 import { getblogData } from "../lib/getblog";
 import Image from "next/image";
+import { BlogText } from "../Homepage/BigText";
+import { usePathname } from "next/navigation";
+import FetchDataSpinner from "../Homepage/FetchDataSpinner";
+import LinkWithArrow from "../Common/LinkWithArrow";
 
 const WhyRapidDigitization = dynamic(() => import("./WhyRapidDigitization"));
 const RapidDigitizationBenefits = dynamic(() =>
@@ -17,6 +21,7 @@ const TechStackWeUse = dynamic(() => import("../Services/TechStackWeUse"));
 const ServicesFAQ = dynamic(() => import("../Services/ServicesFAQ"));
 
 const RapidDigitization = () => {
+  const pathname = usePathname();
   const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1080 });
   const [blogData, setBlogData] = useState(null);
   useEffect(() => {
@@ -94,71 +99,84 @@ const RapidDigitization = () => {
         </div>
     
       </div>
-      <div className="container mx-auto md:!px-3 !px-4">
-            <div className="flex flex-wrap flex-col xl:pb-20 md:pb-14 pb-8">
-              <div className="service_sec3">
-                <p className="home_sec2_txt3 !pb-0 md:!pt-8 !pt-0">
-                  <p className="!ml-0 extra_bold !w-full">
-                    You might also like
-                  </p>
-                </p>
-              </div>
-              <div
-                className={`grid 
-                   xl:grid-cols-3 md:grid-cols-2
-                 grid-cols-1 items-center gap-[2rem]`}
-              >
-                {console.log(blogData)}
-          
-                {  blogData
-                    ?.filter(({ slug }) => true)
-                    ?.slice(0, `${isTablet ? 2 : 3}`)
-                    ?.map(({ slug, name, content }, index) => (
-                      <div
-                        key={index}
-                        className="border-[1px] border-[#80808038] rounded-[30px] blog_flex_30"
-                      >
-                        <Link
-                          as={`/blog/${slug}`}
-                          href={`/blog/[slug]`}
-                          target="_blank"
-                          rel="external"
-                        >
-                          <div className="sec9_img1">
-                            <Image
-                              className="rounded-[30px]"
-                              src={
-                                content?.mobile_banner?.filename
-                                  ? content?.mobile_banner?.filename
-                                  : "/images/not-found-image.webp"
-                              }
-                              alt={
-                                content?.mobile_banner?.alt ||
-                                `Banner-img-${index}`
-                              }
-                              width={550}
-                              height={283}
-                            />
-                          </div>
-                          <div className="pt-[1rem] px-[1rem] pb-[1.5rem] blog-hover">
-                            <div className="border-b-[1px] border-[#80808038] py-[1rem]">
-                              <p className="entry-title default-max-width aspect-[518/116]">
-                                {name}
-                              </p>
-                            </div>
-                            <div className="sec9_txt2 mt-[1.5rem]">
-                              <p className="publish_date">
-                                {formattedDate(content?.Published)}
-                              </p>
-                            </div>
-                          </div>
-                        </Link>
+    <>
+    <div
+      className={
+        pathname === "/" ? "bg-sectionBG section-padding" : "section-padding"
+      }
+    >
+      <div className="container max-w-[1280px] mx-auto reveal">
+        <BlogText />
+        <div className="blog-home reveal">
+          {blogData?.length ? (
+            blogData?.map(({ slug, name, content }, index) => {
+              if (
+                content &&
+                (content.Priority == 1 ||
+                  content.Priority == 2 ||
+                  content.Priority == 3)
+              ) {
+                return (
+                  <div
+                    key={index}
+                    className="blog-box overflow-hidden shadow-none hover:shadow-lg"
+                  >
+                    <Link as={`/blog/${slug}`} href={`/blog/[slug]`}>
+                      <Image
+                        src={content?.mobile_banner?.filename}
+                        alt={content?.mobile_banner?.alt}
+                        className="vc_gitem-zone-img rounded-[20px]"
+                        width={550}
+                        height={283}
+                      />
+                      <div className="p-[10px]">
+                        <h4 className="xl:text-[24px] mb-[10px] leading-8">
+                          {name}
+                        </h4>
                       </div>
-                    ))
+                    </Link>
+                  </div>
+                );
+              } else {
+                return (
+                  <div
+                    key={index}
+                    className="blog-box overflow-hidden shadow-none hover:shadow-lg bg-white"
+                  >
+                    <Link as={`/blog/${slug}`} href={`/blog/[slug]`}>
+                      <Image
+                        src={content?.mobile_banner?.filename || ""}
+                        alt={content?.mobile_banner?.alt || "Blog banner"}
+                        className="vc_gitem-zone-img rounded-[20px]"
+                        width={550}
+                        height={283}
+                        sizes="(min-width: 767px) 550px, calc(100vw - 30px)"
+                      />
+                      <div className="p-[10px]">
+                        <h4 className="xl:text-xl mb-[10px] font-semibold leading-8">
+                          {name}
+                        </h4>
+                      </div>
+                    </Link>
+                  </div>
+                );
               }
-              </div>
+            })
+          ) : (
+            <div className="flex align-middle justify-center">
+              <FetchDataSpinner />
             </div>
-          </div>
+          )}
+        </div>
+        {blogData?.length ? (
+          <LinkWithArrow
+            href="/blog/"
+            label="Read More"
+            className="justify-center"
+          />
+        ) : null}
+      </div>
+    </div></>
           <ServicesFAQ />
       
     </div>
