7da95d1ec563fd66573830a9ede89850f5a03eb5
shivsagar <shiv.s@brilworks.com>
2024-08-07 15:13:26 +0530

    update

--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -266,7 +266,7 @@ export async function fetchData(params) {
     };
 
 
-    const storyUrl = `https://api.storyblok.com/v2/cdn/stories/${slug}?filter_query[component][in]=article&version=${sbParams.version}&resolve_links=${sbParams.resolve_links}&token=${process.env.NEXT_PUBLIC_ACCESS_TOKEN}`;
+    const storyUrl = `https://api.storyblok.com/v2/cdn/stories/${slug}?version=${sbParams.version}&resolve_links=${sbParams.resolve_links}&token=${process.env.NEXT_PUBLIC_ACCESS_TOKEN}`;
     const configUrl = `https://api.storyblok.com/v2/cdn/stories/config?version=${sbParams.version}&token=${process.env.NEXT_PUBLIC_ACCESS_TOKEN}`;
 
     const [storyRes, configRes] = await Promise.all([
