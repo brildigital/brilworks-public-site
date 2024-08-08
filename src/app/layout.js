72fd46609170274ec039e8e869597b29a0d811f3
shivsagar <shiv.s@brilworks.com>
2024-08-08 15:46:05 +0530

    script added  vecotr

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,6 +22,7 @@ storyblokInit({
 
 import Header from "./components/Header/Header"
 import LoadScripts from "./ScriptLoader"
+import Script from "next/script"
 
 const Footer = dynamic(() => import("./components/Footer"));
 
@@ -35,6 +36,12 @@ export default function RootLayout({ children }) {
           {process.env.VERCEL_ENV !== "production" && <meta name="robots" content="noindex,nofollow" />}
         </head>
         <body suppressHydrationWarning={true}>
+        <Script id="vector-script" strategy="afterInteractive">
+        {`
+          !function(e,r){try{if(e.vector)return void console.log("Vector snippet included more than once.");var t={};t.q=t.q||[];for(var o=["load","identify","on"],n=function(e){return function(){var r=Array.prototype.slice.call(arguments);t.q.push([e,r])}},c=0;c<o.length;c++){var a=o[c];t[a]=n(a)}if(e.vector=t,!t.loaded){var i=r.createElement("script");i.type="text/javascript",i.async=!0,i.src="https://cdn.vector.co/pixel.js";var l=r.getElementsByTagName("script")[0];l.parentNode.insertBefore(i,l),t.loaded=!0}}catch(e){console.error("Error loading Vector:",e)}}(window,document);
+          vector.load("1a1e4f1f-0942-4b35-bbad-8ef11726a7e4");
+        `}
+      </Script>
           <Header />
           {/* <NextAuthProvider> */}
             {children}
