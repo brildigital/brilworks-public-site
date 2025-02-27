f994015a563f22185e225888fab60f5e0b070cff
Brilworks Team <digital@brilworks.com>
2025-02-27 18:02:45 +0530

    Changes to autoplay video
    Trigger vercel deploy

--- src/app/components/AISolutions/AISolutions.jsx
+++ src/app/components/AISolutions/AISolutions.jsx
@@ -34,7 +34,7 @@ function AISolutionsFirstSection({
       <div className="video-container">
         <iframe
           className="rounded-3xl"
-          src={videoLink}
+          src={`${videoLink}?autoplay=1&mute=1`}
           title="YouTube video player"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
