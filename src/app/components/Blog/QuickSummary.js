538bbf66dce025ef5bf5eb5ac86ac4ad7a766b9c
Brilworks Team <digital@brilworks.com>
2025-04-17 12:30:30 +0530

    Added Industry pages

--- src/app/components/Blog/QuickSummary.js
+++ src/app/components/Blog/QuickSummary.js
@@ -4,7 +4,7 @@ import React from "react";
 const QuickSummary = ({ data }) => {
   return (
     <div className="post-summary md:max-w-[736px] w-full  text-left ">
-      <span className="font-bold text-themeColor">Quick Summary:-</span>
+      <span className="font-bold text-themeColor">Quick Summary:- </span>
       {data}
     </div>
   );
