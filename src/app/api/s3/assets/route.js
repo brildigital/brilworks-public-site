454482df7f62a9445c6a75a2cb37240f473a5286
Manish5900 <manish.s@brilworks.com>
2025-05-23 15:59:54 +0530

    Changes for s3 files upload feature

--- src/app/api/s3/assets/route.js
+++ src/app/api/s3/assets/route.js
@@ -1,5 +1,11 @@
 import { NextResponse } from "next/server";
-import { S3Client, ListObjectsV2Command, _Object } from "@aws-sdk/client-s3";
+import {
+  S3Client,
+  ListObjectsV2Command,
+  _Object,
+  PutObjectCommand,
+} from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 const s3 = new S3Client({
   region: process.env.AWS_REGION,
@@ -57,6 +63,47 @@ export async function GET() {
     console.error("Error listing S3 objects:", error);
     return NextResponse.json(
       { error: "Failed to fetch documents" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(req) {
+  try {
+    // Parse formData from the request
+    const formData = await req.formData();
+    const name = formData.get("name");
+    const type = formData.get("type");
+
+    if (!name || !type) {
+      return NextResponse.json(
+        { error: "Missing name or type in formData" },
+        { status: 400 }
+      );
+    }
+
+    const timestamp = Date.now();
+    const fileExtension = name.split(".").pop();
+    const baseName = name.replace(/\s+/g, "_").replace(/\.[^.]+$/, "");
+    const fileKey = `public/${baseName}-${timestamp}.${fileExtension}`;
+
+    const command = new PutObjectCommand({
+      Bucket: process.env.AWS_S3_BUCKET,
+      Key: fileKey,
+      ContentType: type,
+    });
+
+    const signedUrl = await getSignedUrl(s3, command, { expiresIn: 60 });
+
+    const response = NextResponse.json({
+      uploadUrl: signedUrl,
+      fileUrl: `https://${process.env.AWS_S3_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com/${fileKey}`,
+    });
+    return response;
+  } catch (err) {
+    console.error("Error generating pre-signed URL:", err);
+    return NextResponse.json(
+      { error: "Failed to generate upload URL" },
       { status: 500 }
     );
   }
