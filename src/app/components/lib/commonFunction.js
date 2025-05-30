6bba49110ef0f80a2320b929e4911a62b33a89a3
Manish5900 <manish.s@brilworks.com>
2025-05-30 18:59:37 +0530

    Changes to resolve cors error

--- src/app/components/lib/commonFunction.js
+++ src/app/components/lib/commonFunction.js
@@ -285,25 +285,20 @@ export function formatTitleFromUrl(url) {
     .join(" "); // Join words with spaces
 }
 export function formatSrcUrl(url) {
-  const s3Region = process.env.NEXT_PUBLIC_AWS_REGION;
-  const s3Bucket = process.env.NEXT_PUBLIC_AWS_S3_BUCKET;
-  if (s3Region && s3Bucket) {
-    const urlParts = url.split("/");
+  const urlParts = url.split("/");
 
-    // Get the asset ID (second last segment)
-    const assetId = urlParts[urlParts.length - 2];
+  // Get the asset ID (second last segment)
+  const assetId = urlParts[urlParts.length - 2];
 
-    // Get the filename (last segment)
-    const filename = urlParts[urlParts.length - 1];
+  // Get the filename (last segment)
+  const filename = urlParts[urlParts.length - 1];
 
-    // Construct new S3 file name
-    const newFileName =
-      assetId !== "public" || assetId !== "assets"
-        ? `${assetId}_${filename}`
-        : `${filename}`;
+  // Construct new S3 file name
+  const newFileName =
+    assetId !== "public" ? `${assetId}_${filename}` : `${filename}`;
 
-    const fileURL = `https://${s3Bucket}.s3.${s3Region}.amazonaws.com/assets/${newFileName}`;
+  const fileURL = `https://d11qzsb0ksp6iz.cloudfront.net/assets/${newFileName}`;
+  // const fileURL = `https://${s3Bucket}.s3.${s3Region}.amazonaws.com/assets/${newFileName}`;
 
-    return fileURL;
-  }
+  return fileURL;
 }
