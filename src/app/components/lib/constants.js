2ac6634f4b9fa9181d38f5e1f9e67a2cbf5d0ede
Brilworks Team <digital@brilworks.com>
2025-08-14 16:29:59 +0530

    Updated sitemap and added industry in footer

--- src/app/components/lib/constants.js
+++ src/app/components/lib/constants.js
@@ -900,6 +900,26 @@ export const sitemapData = [
 
 export const blogListStaticData = [
   {
+    name: "Comparing Node.js with Top Backend Technologies",
+    loc: "https://www.brilworks.com/blog/node-js-vs-top-backend-technologies/",
+    lastmod: "2025-08-14T06:50:19.205Z",
+  },
+  {
+    name: "Top Mobile App Development Frameworks and How They Compare",
+    loc: "https://www.brilworks.com/blog/mobile-app-development-frameworks/",
+    lastmod: "2025-08-13T12:29:33.695Z",
+  },
+  {
+    name: "How To Find And Hire Top Node.js Developers In 2025",
+    loc: "https://www.brilworks.com/blog/where-to-find-top-node-js-developers/",
+    lastmod: "2025-08-11T13:14:16.864Z",
+  },
+  {
+    name: "Why Java Is Still the Backbone of Fintech App Development",
+    loc: "https://www.brilworks.com/blog/java-in-fintech-app-development/",
+    lastmod: "2025-08-11T12:50:38.794Z",
+  },
+  {
     name: "Why Node.js Is a Strategic Choice for Healthcare",
     loc: "https://www.brilworks.com/blog/nodejs-healthcare-strategic-technology-choice/",
     lastmod: "2025-08-08T13:56:27.492Z",
