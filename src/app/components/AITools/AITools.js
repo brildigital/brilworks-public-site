ebc931fb740a86b8cdf6ee7e43ea299e9ffe0bda
Brilworks Team <digital@brilworks.com>
2025-10-07 09:48:46 +0530

    Added list images

--- src/app/components/AITools/AITools.js
+++ src/app/components/AITools/AITools.js
@@ -8,7 +8,7 @@ const tools = [
     title: "AI Insights & Automation",
     description:
       "Automate workflows with n8n and AI. Connect apps, streamline processes, and reduce manual work with smart, customizable automation.",
-    image: "/images/v2/n8n-ai.webp",
+    image: "/images/v2/ai-automation.webp",
     link: "/ai-tools/ai-insights-and-automation",
     bg: "bg-[#EA4B71] text-white",
   },
@@ -16,7 +16,7 @@ const tools = [
     title: "AI Chatbot & Conversational Solution",
     description:
       "Engage customers with intelligent AI-powered chatbots. Deliver human-like conversations for support, sales, and onboarding—available 24/7.",
-    image: "/images/ai-bot.png",
+    image: "/images/v2/ai-conversational.webp",
     link: "/ai-tools/ai-chatbot-conversational-solution",
     bg: "bg-themeColor text-white",
   },
@@ -24,7 +24,7 @@ const tools = [
     title: "Pre-Build Solutions",
     description:
       "Access ready-to-deploy AI solutions built for speed and efficiency. Save time with pre-configured tools for business, marketing, and productivity.",
-    image: "/images/prebuild-solutions.png",
+    image: "/images/v2/prebuild-sol.webp",
     link: "/ai-tools/pre-build-solutions",
     bg: "bg-green-600 text-white",
   },
@@ -32,7 +32,7 @@ const tools = [
     title: "AWS Partner Solution",
     description:
       "Leverage our expertise as an official AWS Partner. Scale applications, optimize cloud infrastructure, and integrate AI solutions with enterprise-grade reliability.",
-    image: "/images/aws-partner.png",
+    image: "/images/v2/aws-partner-sol.webp",
     link: "/ai-tools/aws-partner-solution",
     bg: "bg-[#ff9900] text-white",
   },
@@ -40,7 +40,7 @@ const tools = [
     title: "MVP in 48 Hours",
     description:
       "Validate your startup idea fast. Get a working Minimum Viable Product in just 48 hours, powered by AI and rapid prototyping.",
-    image: "/images/mvp.png",
+    image: "/images/v2/mvp-in-48-hr.webp",
     link: "/mvp-in-48-hours/",
     bg: "bg-navyBlue text-white",
   },
@@ -48,7 +48,7 @@ const tools = [
     title: "Free Mockups",
     description:
       "Access free, high-quality design mockups to showcase your brand, apps, or websites. Download and use instantly for presentations and pitches.",
-    image: "/images/mockups.png",
+    image: "/images/v2/fee-mockups-ai-tool.webp",
     link: "/free-mockups/",
     bg: "bg-themeSecondary text-white",
   },
@@ -91,27 +91,27 @@ const AITools = () => {
               href={tool.link}
               className="cursor-pointer rounded-xl shadow-md hover:shadow-xl transition-shadow bg-white overflow-hidden group"
             >
-              {/* <img
+              <img
                 src={tool.image}
                 alt={tool.title}
-                className="w-full h-40 object-cover group-hover:scale-105 transition-transform"
-              /> */}
+                className="w-full h-[240px] object-cover group-hover:scale-105 transition-transform"
+              />
 
-              <div
+              {/* <div
                 className={`${tool.bg} flex items-center justify-center p-6`}
               >
                 <div className="w-72 h-40 flex items-center justify-center">
-                  {/* <Image
+                   <Image
                     src={tool.image}
                     alt={tool.title}
                     fill
                     className="object-contain"
-                  /> */}
+                  /> 
                   <p className="text-2xl md:text-3xl lg:text-4xl font-bold text-center ">
                     {tool?.title}
                   </p>
                 </div>
-              </div>
+              </div>*/}
               <div className="p-4">
                 <h3 className="text-xl font-semibold text-gray-800 mb-3">
                   {tool.title}
