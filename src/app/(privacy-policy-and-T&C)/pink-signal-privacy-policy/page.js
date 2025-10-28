7604159d8b79fe11d0205e9a767e9048398dae82
Manish5900 <manish.s@brilworks.com>
2025-10-28 14:36:23 +0530

    Added PinkSinal privacy policy page

---
+++ src/app/(privacy-policy-and-T&C)/pink-signal-privacy-policy/page.js
@@ -0,0 +1,30 @@
+import dynamic from "next/dynamic";
+const PinkSignalPrivacyPolicy = dynamic(() =>
+  import("@/app/components/PrivacyPolicy/PinkSignalPrivacyPolicy")
+);
+
+export async function getPrivacyPolicy() {
+  try {
+    const storyUrl = `https://api.storyblok.com/v2/cdn/stories/pink-signal-privacy-policy?version=${process.env.NEXT_PUBLIC_STORYBLOK_VERSION}&token=${process.env.NEXT_PUBLIC_ACCESS_TOKEN}`;
+    const storyRes = await fetch(storyUrl, { next: { revalidate: 0 } });
+    const storyData = await storyRes.json();
+    return storyData?.story;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return null;
+  }
+}
+
+export default async function page() {
+  const privacyPolicyData = await getPrivacyPolicy();
+  return (
+    <>
+      {process.env.VERCEL_ENV === "production" ? (
+        <head>
+          <meta name="robots" content="noindex, nofollow" />
+        </head>
+      ) : null}
+      <PinkSignalPrivacyPolicy data={privacyPolicyData} />
+    </>
+  );
+}
